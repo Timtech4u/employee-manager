da24734affbdfb9046c045bf3e92b7a875aa8589
Tim <timothyolaleke@gmail.com>
2018-08-27 17:29:05 +0100

    Initial codes with Django tenant schemas package

---
+++ format_index_html.py
@@ -0,0 +1,16 @@
+import sys
+import fileinput
+
+file = 'templates/index.html'
+
+with open(file, "r+") as f:
+    s = f.read()
+    f.seek(0)
+    f.write("{% load staticfiles %}\n" + s)
+
+for i, line in enumerate(fileinput.input(file, inplace=1)):
+    sys.stdout.write(line.replace('/static-vuedj/', "{% static '"))
+for i, line in enumerate(fileinput.input(file, inplace=1)):
+    sys.stdout.write(line.replace('.css', ".css' %}"))
+for i, line in enumerate(fileinput.input(file, inplace=1)):
+    sys.stdout.write(line.replace('.js', ".js' %}"))
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <br>
+    <img class="logo" src="./assets/dj.png">
+    <router-view/>
+    <p>
+      Welcome to your Django - Vue.js app!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
