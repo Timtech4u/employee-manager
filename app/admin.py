b84b34359b855de9629a36a71848fbec8c6d610f
Tim <timothyolaleke@gmail.com>
2018-08-28 17:32:14 +0100

    Added profile and client to Admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Profile
+from customers.models import Client
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Client)
