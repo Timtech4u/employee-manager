ea6c653c4a12f925bcd5b4bf278c380f51629b8e
Tim <timothyolaleke@gmail.com>
2018-09-11 02:23:23 +0100

    Removed unnecessary modules

--- src/main.js
+++ src/main.js
@@ -4,17 +4,6 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-import 'bulma/css/bulma.css'
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
-
-import lang from 'element-ui/lib/locale/lang/en'
-import locale from 'element-ui/lib/locale'
-
-locale.use(lang)
-
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
